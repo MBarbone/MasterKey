d24495e15c8e43903ac086e4238c39d92e76194b
Mike <michael.barbone@gmail.com>
2019-05-05 17:45:44 -0400

    Add bodyParser and fix modal bug

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("express-async-errors");
 const helmet = require("helmet");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const adminHtml = require("./routes/admin-html-routes");
 const payments = require("./routes/payments");
 const services = require("./routes/services");
@@ -16,8 +17,8 @@ const app = express();
 const db = require("./models");
 
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // html routes
