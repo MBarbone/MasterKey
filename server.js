bb11adc5dff7ea6817f86bcb22fbd30404dd8a92
Mike <michael.barbone@gmail.com>
2019-05-05 19:00:23 -0400

    Create login select page and different login pages. Majority of handlebars rendering working.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("express-async-errors");
 const helmet = require("helmet");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+const path = require("path");
 const adminHtml = require("./routes/admin-html-routes");
 const payments = require("./routes/payments");
 const services = require("./routes/services");
@@ -33,6 +34,18 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/html/index.html"));
+});
+
+app.get("/tenant/login", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/user-login.html"));
+});
+
+app.get("/admin/login", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/admin-login.html"));
+});
+
 app.get("/tenant/home", async (req, res) => {
   res.render("tenantHome");
 });
@@ -47,6 +60,22 @@ app.get("/admin/home", async (req, res) => {
 
 app.get("/admin/building", async (req, res) => {
   res.render("buildingTenants");
+});
+
+app.get("/tenant/home", async (req, res) => {
+  res.render("tenantHome");
+});
+
+app.get("/admin/payments", async (req, res) => {
+  res.render("adminPayments");
+});
+
+app.get("/tenant/services", async (req, res) => {
+  res.render("tenantService");
+});
+
+app.get("/tenant/payments", async (req, res) => {
+  res.render("tenantPayment");
 });
 
 db.sequelize.sync({ force: true }).then(function() {
